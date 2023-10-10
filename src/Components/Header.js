24777454c3830e277363b35e0cadfa050c6e0712
edm1001 <edmer_franciz@yahoo.com>
2023-10-09 19:41:06 -0500

    add logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,13 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Logo from '../assets/aquamania-logo.gif'
+import Container from 'react-bootstrap/Container';
+import Logo from '../assets/aquamania-logo.gif';
 
 const Header = () => {
     return (
-      <Navbar className="" sticky="top" bg="dark" data-bs-theme="light" expand="lg" >
+      <Navbar className="" sticky="top" bg="light" data-bs-theme="light" expand="lg" >
+        <Container>
+
         <Navbar.Brand>
           <img 
           src={Logo}
@@ -14,12 +17,14 @@ const Header = () => {
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-        
+        <Navbar.Collapse>
         <Nav>
         <Nav.Link>Home</Nav.Link>
         <Nav.Link>Fish Care</Nav.Link>
         <Nav.Link>Products</Nav.Link>
         </Nav>
+        </Navbar.Collapse>
+        </Container>
       </Navbar>
     )
 }
