c07b0e5ca2ca3285ab38ac7276b42b9ef680cb48
edm1001 <edmer_franciz@yahoo.com>
2023-10-10 19:45:25 -0500

    navbar working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,16 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import Logo from '../assets/aquamania-logo.gif';
 
 const Header = () => {
     return (
-      <Navbar className="" sticky="top" bg="light" data-bs-theme="light" expand="lg" >
+      <Navbar  sticky="top" bg="light" data-bs-theme="light" expand="lg" >
         <Container>
-
         <Navbar.Brand>
           <img 
+          className=''
           src={Logo}
           width="80"
           height="80"
@@ -19,13 +20,14 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Collapse>
         <Nav>
-        <Nav.Link>Home</Nav.Link>
-        <Nav.Link>Fish Care</Nav.Link>
-        <Nav.Link>Products</Nav.Link>
+        <Nav.Link>Fresnwater</Nav.Link>
+        <Nav.Link>Saltwater</Nav.Link>
+        <Nav.Link>Searchbar</Nav.Link>
         </Nav>
         </Navbar.Collapse>
         </Container>
       </Navbar>
+      
     )
 }
 
