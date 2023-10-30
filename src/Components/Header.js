12d4225d842cd0c48128e634abf961479f9ffa33
edm1001 <edmer_franciz@yahoo.com>
2023-10-29 23:22:11 -0500

    refactor welcome

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,45 +1,31 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import Logo from '../assets/aquamania-logo.gif';
 
 const Header = () => {
     return (
-      <Navbar  className='' sticky="top" expand="md" >
+      <Navbar className="" sticky="top" bg="light" data-bs-theme="light" expand="lg" >
         <Container>
+
         <Navbar.Brand>
           <img 
-          className=''
           src={Logo}
           width="80"
           height="80"
           alt='brnd-logo'
           />
         </Navbar.Brand>
-
         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Collapse>
-        <Nav
-        className='ms-5 ps-5'
-        >
-        <NavDropdown title='Freshwater' className=''  >
-        <NavDropdown.Item>Aggressive</NavDropdown.Item>
-          <NavDropdown.Item>Semi-Aggresive</NavDropdown.Item>
-          <NavDropdown.Item>Peaceful</NavDropdown.Item>
-          </NavDropdown>
-
-        <NavDropdown title='Saltwater'  >
-          <NavDropdown.Item>Aggressive</NavDropdown.Item>
-          <NavDropdown.Item>Semi-Aggresive</NavDropdown.Item>
-          <NavDropdown.Item>Peaceful</NavDropdown.Item>
-          </NavDropdown>
-        <Nav.Link>About Us</Nav.Link>
+        <Nav>
+        <Nav.Link className='text-center'>Home</Nav.Link>
+        <Nav.Link className='text-center'>Fish Care</Nav.Link>
+        <Nav.Link className='text-center'>Products</Nav.Link>
         </Nav>
         </Navbar.Collapse>
         </Container>
       </Navbar>
-      
     )
 }
 
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Button, Form, Carousel, Image } from "react-bootstrap";
+import { Container, Row, Col, Button, Form, Carousel, Card } from "react-bootstrap";
 import Placeholder from '../assets/photos/placeholder.png';
 
 const Welcome = () => {
@@ -18,45 +18,44 @@ const Welcome = () => {
   ];
 
   return (
-      <Container fluid>
-          <Row className="bg-info">
-              <Col>
-                  <h4 className="justify-content-center align-center fw-bold pt-1">
-                      Search Topics:
-                  </h4>
-              </Col>
+    <Container fluid>
+        <Row className="bg-info">
+            <Col>
+                <h4 className="justify-content-center align-center fw-bold pt-1">
+                    Search Topics:
+                </h4>
+            </Col>
 
-              <Col>
-                  <Row>
-                      <Col className="px-0" lg={8} md={8} sm={8} xs={8}>
-                          <Form.Control
-                              type="text"
-                              placeholder="Search"
-                              className=""
-                          />
-                      </Col>
-                      <Col className='ps-0 ' lg={4} md={4} sm={4} xs={4}>
-                          <Button type="submit">Submit</Button>
-                      </Col>
-                  </Row>
-              </Col>
-          </Row>
+            <Col>
+                <Row>
+                    <Col className="px-0" lg={8} md={8} sm={8} xs={8}>
+                        <Form.Control
+                            type="text"
+                            placeholder="Search"
+                            className=""
+                        />
+                    </Col>
+                    <Col className='ps-0 ' lg={4} md={4} sm={4} xs={4}>
+                        <Button type="submit">Submit</Button>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
 
-          <Row>
-              <Carousel>
-                  {searchTopics.map((topic, index) => (
-                      <Carousel.Item key={index}>
-                          <img src={Placeholder} text="First slide" alt="welcome-img" className="d-block w-100 h-100" />
-                          <Carousel.Caption>
-                              <h3>{topic.label}</h3>
-                              <p>{topic.description}</p>
-                          </Carousel.Caption>
-                      </Carousel.Item>
-                  ))}
-              </Carousel>
-          </Row>
-      </Container>
-  )
+        <Row>
+            <Carousel>
+                {searchTopics.map((topic, index) => (
+                    <Carousel.Item key={index}>
+                        <img src={Placeholder} text="First slide" alt="welcome-img" className="d-block w-100 h-100" />
+                        <Carousel.Caption>
+                            <h3>{topic.label}</h3>
+                            <p>{topic.description}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        </Row>
+    </Container>
+)
 }
-
 export default Welcome;
