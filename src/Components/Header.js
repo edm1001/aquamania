cfc6b23ae995c7aa90816eba9bcd581894ecc515
edm1001 <edmer_franciz@yahoo.com>
2023-10-30 12:02:52 -0500

    refactor navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Logo from '../assets/aquamania-logo.gif';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Header = () => {
     return (
       <Navbar className="" sticky="top" bg="light" data-bs-theme="light" expand="lg" >
         <Container>
-
         <Navbar.Brand>
           <img 
           src={Logo}
@@ -18,9 +18,20 @@ const Header = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Collapse>
-        <Nav>
-        <Nav.Link className='text-center'>Home</Nav.Link>
-        <Nav.Link className='text-center'>Fish Care</Nav.Link>
+        <Nav className='mr-auto'>
+        <NavDropdown title="Freshwater">
+              <NavDropdown.Item>By Species</NavDropdown.Item>
+              <NavDropdown.Item>Aggressive</NavDropdown.Item>
+              <NavDropdown.Item>Semi-Aggressive</NavDropdown.Item>
+              <NavDropdown.Item>Peaceful</NavDropdown.Item>
+            </NavDropdown>
+        <NavDropdown title="Saltwater">
+              <NavDropdown.Item>By Species</NavDropdown.Item>
+              <NavDropdown.Item>Aggressive</NavDropdown.Item>
+              <NavDropdown.Item>Semi-Aggressive</NavDropdown.Item>
+              <NavDropdown.Item>Peaceful</NavDropdown.Item>
+            </NavDropdown> 
+        <Nav.Link className='text-center'>About Us</Nav.Link>
         <Nav.Link className='text-center'>Products</Nav.Link>
         </Nav>
         </Navbar.Collapse>
