a68e893da81bd6ef808e5dff2f029cdf6beaacf2
edm1001 <edmer_franciz@yahoo.com>
2023-10-29 22:43:29 -0500

    refactor search topc section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
 const About = () => {
+    const fadeIn = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 }
+    };
+
     return (
-        <div>
-            hello About
-            
-        </div>
-    )
+        <motion.div
+            variants={fadeIn}
+            initial="hidden"
+            animate="visible"
+        >
+            <h2>About Aquamania</h2>
+            <p>Welcome to Aquamania - Your Ultimate Hobbyist Hub for Aquatic Enthusiasts!</p>
+            <p>At Aquamania, we're your go-to source for everything related to aquatic hobbies. Whether you're into freshwater or marine aquariums, pond keeping, or aquascaping, we're dedicated to providing you with the best information, tips, and community support.</p>
+            <p>Dive into the world of aquatic wonders with fellow hobbyists. Whether you're just starting out or a seasoned pro, we're here to share the excitement and knowledge with you.</p>
+            <p>Feel free to get in touch with us to learn more about how we can support your aquatic hobby needs.</p>
+        </motion.div>
+    );
 }
 
 export default About;
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -2,64 +2,61 @@ import { Container, Row, Col, Button, Form, Carousel, Image } from "react-bootst
 import Placeholder from '../assets/photos/placeholder.png';
 
 const Welcome = () => {
-    return (
-    <Container fluid >
-      <Row 
-            className="
-            bg-info 
-            ">
-            <Col>
-            <h4 className="justify-content-center align-center fw-bold pt-1">
-            Search Topics:
-            </h4>
+  const searchTopics = [
+    {
+      label: "Freshwater",
+      description: "Explore freshwater aquariums with a variety of fish and plants. Get maintenance tips."
+  },
+  {
+      label: "Saltwater",
+      description: "Dive into stunning saltwater aquariums, coral reefs, and exotic fish."
+  },
+  {
+      label: "Fish Care",
+      description: "Learn essential fish care, from feeding to health."
+      }
+  ];
+
+  return (
+      <Container fluid>
+          <Row className="bg-info">
+              <Col>
+                  <h4 className="justify-content-center align-center fw-bold pt-1">
+                      Search Topics:
+                  </h4>
               </Col>
-                
-        <Col>
-        <Row>
-            <Col className="px-0" lg={8} md={8} sm={8} xs={8}>
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className=""
-              />
-          </Col>
-          <Col className='ps-0 'lg={4} md={4} sm={4} xs={4} >
-            <Button type="submit">Submit</Button>
-          </Col>
-            </Row>
-        </Col>
-    </Row>
 
-      <Row>
-        <Carousel>
-      <Carousel.Item>
-        <img src={Placeholder} text="First slide" alt="welcome-img" className="d-block w-100 h-100"/>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+              <Col>
+                  <Row>
+                      <Col className="px-0" lg={8} md={8} sm={8} xs={8}>
+                          <Form.Control
+                              type="text"
+                              placeholder="Search"
+                              className=""
+                          />
+                      </Col>
+                      <Col className='ps-0 ' lg={4} md={4} sm={4} xs={4}>
+                          <Button type="submit">Submit</Button>
+                      </Col>
+                  </Row>
+              </Col>
+          </Row>
 
-      <Carousel.Item>
-      <img src={Placeholder} text="First slide" alt="welcome-img" className="d-block w-100 h-100"/>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={Placeholder} text="First slide" alt="welcome-img" className="d-block w-100 h-100"/>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  </Row>  
-  </Container>
-    )
+          <Row>
+              <Carousel>
+                  {searchTopics.map((topic, index) => (
+                      <Carousel.Item key={index}>
+                          <img src={Placeholder} text="First slide" alt="welcome-img" className="d-block w-100 h-100" />
+                          <Carousel.Caption>
+                              <h3>{topic.label}</h3>
+                              <p>{topic.description}</p>
+                          </Carousel.Caption>
+                      </Carousel.Item>
+                  ))}
+              </Carousel>
+          </Row>
+      </Container>
+  )
 }
 
 export default Welcome;
