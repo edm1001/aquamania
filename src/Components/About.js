68933e9daaec296566f93b0864cbcecbe856fb4f
edm1001 <edmer_franciz@yahoo.com>
2023-11-09 01:14:14 -0600

    add more to db

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import Badge  from 'react-bootstrap/Badge';
+
 
 const About = () => {
     const fadeIn = {
@@ -8,23 +10,24 @@ const About = () => {
     };
 
     return (
+        
         <motion.div
             variants={fadeIn}
             initial="hidden"
             animate="visible"
-            className='text-center h-100 w-100 py-5'
+            className='text-center h-100 w-100 py-5 px-3 bg-success text-white'
         >
-            <h2>About Aquamania</h2>
-            <motion.div
-                initial={{opacity:0}}
-                whileInView={{opacity:1,transition:1}}
-                className='py-2'
-            >
-
-            <p>Welcome to Aquamania - Your Ultimate Hobbyist Hub for Aquatic Enthusiasts!<br/>
-            As a Fish hobbyist I created this site as a go-to source for our aquatic hobby. Whether you're into freshwater or marine aquariums, pond keeping, or aquascaping, we're dedicated to providing you with the best information, tips, and community support.<br/>
-            Dive into the world of aquatic wonders with fellow hobbyists. Whether you're just starting out or a seasoned pro, we're here to share the excitement and knowledge with you. Get in touch with us to learn more!</p>
-            </motion.div>
+            <h1 className='fw-bold text-warning'>About Aquamania</h1>
+        <motion.div
+            initial={{opacity:0}}
+            whileInView={{opacity:1,transition:1}}
+            className='py-2'
+        >
+            <p>
+            Aquamania was created as a go-to source for the aquatic hobby, to help fish owners by anticipating their specific fish, whether you're into freshwater or marine aquariums, this site is dedicated to provide the best information & tips!<br/>
+            Whether starting out or a seasoned pro, we're here to share the passion and knowledge with all. </p>
+            <Badge pill bg="info">Get in touch with us to be featured!</Badge>
+        </motion.div>
         </motion.div>
     );
 }
