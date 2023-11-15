e63c5cdbd971d2b2ae6094cd8c8e4e17a791f346
edm1001 <edmer_franciz@yahoo.com>
2023-11-15 17:48:50 -0600

    add goldfish

--- src/db/FeshwaterFishData.js
+++ src/db/FeshwaterFishData.js
@@ -1,5 +1,5 @@
 // FreshwaterFishData.js
-// add cichlid, platy, swordtail, catfish, oddballs
+// add cichlid, platy, catfish, oddballs
 const freshwaterFishSpecies = [
   // Tetra
   {
@@ -356,6 +356,173 @@ const freshwaterFishSpecies = [
     waterPHRange: '7.0 - 8.5',
     temperatureRange: '72°F - 78°F (22°C - 26°C)'
   },
+  // GOLDFISH
+  {
+    name: "Veiltail Goldfish",
+    description: "The Veiltail Goldfish is known for its long, flowing double tail that gives it an elegant appearance.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 6 inches (15 cm)",
+    img: "https://example.com/veiltail-goldfish-image.jpg",
+    size: "Small to Medium",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Beginner",
+    waterPHRange: "6.0 - 8.0",
+    temperatureRange: "65°F - 75°F (18°C - 24°C)"
+  },
+  {
+    name: "Common Goldfish",
+    description: "The Common Goldfish is the classic goldfish variety with a sleek, streamlined body and vibrant coloration.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 12 inches (30 cm)",
+    img: "https://example.com/common-goldfish-image.jpg",
+    size: "Medium to Large",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Beginner",
+    waterPHRange: "6.0 - 8.0",
+    temperatureRange: "65°F - 75°F (18°C - 24°C)"
+  },
+  {
+    name: "Tamasaba Goldfish",
+    description: "The Tamasaba Goldfish is recognized for its elongated body and distinct forked tail, resembling a traditional Japanese fan.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Around 10 inches (25 cm)",
+    img: "https://example.com/tamasaba-goldfish-image.jpg",
+    size: "Medium",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Intermediate",
+    waterPHRange: "6.5 - 7.5",
+    temperatureRange: "60°F - 75°F (15°C - 24°C)"
+  },
+  {
+    name: "Bubble Eye Goldfish",
+    description: "The Bubble Eye Goldfish is characterized by its fluid-filled sacs or bubbles underneath its eyes, creating a distinctive appearance.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 6 inches (15 cm)",
+    img: "https://example.com/bubble-eye-goldfish-image.jpg",
+    size: "Small to Medium",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Intermediate",
+    waterPHRange: "6.0 - 8.0",
+    temperatureRange: "68°F - 74°F (20°C - 23°C)"
+  },
+  {
+    name: "Telescope Goldfish",
+    description: "The Telescope Goldfish is known for its protruding, telescope-like eyes, giving it a distinctive and charming appearance.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 8 inches (20 cm)",
+    img: "https://example.com/telescope-goldfish-image.jpg",
+    size: "Small to Medium",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Intermediate",
+    waterPHRange: "6.5 - 7.5",
+    temperatureRange: "68°F - 74°F (20°C - 23°C)"
+  },
+  {
+    name: "Oranda Goldfish",
+    description: "The Oranda Goldfish is distinguished by the prominent hood or wen on its head, which can develop into a large, fleshy growth.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 8 inches (20 cm)",
+    img: "https://example.com/oranda-goldfish-image.jpg",
+    size: "Medium",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Intermediate",
+    waterPHRange: "6.5 - 7.5",
+    temperatureRange: "68°F - 74°F (20°C - 23°C)"
+  },
+  {
+    name: "Comet Goldfish",
+    description: "The Comet Goldfish is characterized by its long, flowing tail fin and vibrant coloration, resembling a comet streaking through the sky.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 12 inches (30 cm)",
+    img: "https://example.com/comet-goldfish-image.jpg",
+    size: "Medium to Large",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Beginner",
+    waterPHRange: "6.0 - 8.0",
+    temperatureRange: "65°F - 75°F (18°C - 24°C)"
+  },  
+  {
+    name: "Lionhead Goldfish",
+    description: "The Lionhead Goldfish is known for its lack of a dorsal fin and the development of a raspberry-like growth on the top of its head.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 8 inches (20 cm)",
+    img: "https://example.com/lionhead-goldfish-image.jpg",
+    size: "Small to Medium",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Intermediate",
+    waterPHRange: "6.5 - 7.5",
+    temperatureRange: "68°F - 74°F (20°C - 23°C)"
+  },
+  {
+    name: "Ryukin Goldfish",
+    description: "The Ryukin Goldfish has a distinctive hump on its back, a pointed caudal fin, and a deep body, making it a visually striking variety.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 8 inches (20 cm)",
+    img: "https://example.com/ryukin-goldfish-image.jpg",
+    size: "Small to Medium",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Intermediate",
+    waterPHRange: "6.5 - 7.5",
+    temperatureRange: "68°F - 74°F (20°C - 23°C)"
+  },
+  {
+    name: "Shubunkin Goldfish",
+    description: "The Shubunkin Goldfish is known for its calico coloration, which includes patches of red, orange, blue, and black, creating a vibrant and unique appearance.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 10 inches (25 cm)",
+    img: "https://example.com/shubunkin-goldfish-image.jpg",
+    size: "Medium",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Beginner",
+    waterPHRange: "6.0 - 8.0",
+    temperatureRange: "65°F - 75°F (18°C - 24°C)"
+  },
+  {
+    name: "Celestial Eye Goldfish",
+    description: "The Celestial Eye Goldfish has eyes that are pointed upwards, giving it a unique and celestial appearance.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 6 inches (15 cm)",
+    img: "https://example.com/celestial-eye-goldfish-image.jpg",
+    size: "Small",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Intermediate",
+    waterPHRange: "6.5 - 7.5",
+    temperatureRange: "68°F - 74°F (20°C - 23°C)"
+  },
+  {
+    name: "Ranchu Goldfish",
+    description: "The Ranchu Goldfish is known for its rounded body, lack of a dorsal fin, and a distinctive head shape, creating a charming and unique appearance.",
+    temperament: "Peaceful",
+    maxGrowthSize: "Up to 6 inches (15 cm)",
+    img: "https://example.com/ranchu-goldfish-image.jpg",
+    size: "Small",
+    species: "Carassius auratus",
+    family: "Goldfish",
+    experienceLevel: "Intermediate",
+    waterPHRange: "6.5 - 7.5",
+    temperatureRange: "68°F - 74°F (20°C - 23°C)"
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 ];
 
   export default freshwaterFishSpecies;
