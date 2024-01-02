6ee0a54463e21e42aababee93e6ed7f4d592c1a4
edm1001 <edmer_franciz@yahoo.com>
2024-01-01 21:01:26 -0600

    edit blog page

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -4,18 +4,29 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default function Blog() {
   return (
     <>
-    <Container>
+    <Container className="text-center">
     <h2>Talk about anything Aqua!</h2>
     <p>We encourage this to be a safe space to debate or talk amongst fish lovers, any hateful or insulting comments will result in deleted comment, topic or account termination.</p>
     </Container>
 
     <Container>
       <Row>
-        <Col>
-        <h4>Topics:</h4>
+        <Col as='div' className='w-100' >
+        <h3>Topics:</h3>
+        <ul className='subject-list'>
+        <li><a href='#tank'>Tank Maintenance</a></li>
+        <li><a href='#car'>Fish care</a></li>
+        <li><a href='#fre'>Freshwater</a></li>
+        <li><a href='#salt'>Saltwater</a></li>
+        </ul>
         </Col>
-        <Col>
-        <h4>Most Popular</h4>
+        <Col as='div'>
+        <h3>Most Popular:</h3>
+        <ul>
+          <li>topic1</li>
+          <li>topic2</li>
+          <li>topic3</li>
+        </ul>
         </Col>
       </Row>
     </Container>
