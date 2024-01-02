b3ac4ce5c4f157328281eb687d27762c10b228df
edm1001 <edmer_franciz@yahoo.com>
2024-01-01 21:01:26 -0600

    edit blog page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from './Components/Header.js';
 import Home from './Pages/Home.js'
-import Care from './Pages/Care.js';
 import Products from './Pages/Products.js';
 import Footer from './Components/Footer.js'
 import FreshPage from './Pages/FreshwaterPage.js';
@@ -19,7 +18,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/freshpage" element={<FreshPage/>} /> 
           <Route path="/saltpage" element={<SaltPage/>} /> 
-          <Route path="/care" element={<Care/>}/>
           <Route path="/products" element={<Products/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/blog" element={<Blog/>}/>
