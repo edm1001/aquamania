45640184eff34b1b04d8df2440f41dd7021c8e30
edm1001 <edmer_franciz@yahoo.com>
2024-01-01 18:15:22 -0600

    init blog

---
+++ src/db/Salt/SaltwaterFishData.js
@@ -0,0 +1,8 @@
+// Angelfish, anglers, anthias, basslets, batfish, blennies, boxfish, butterflyfish, cardinal fish, chromis, clownfish, dartfish, dottyback, dragonets, eels, filefish, foxface, gobies, groupers, puffers, rays, hawffish, hogfish, jawfish, lionfish, pipefish, seahorses, sharks, squirrelfish, tangs
+
+const SaltwaterFishData = [
+
+    
+
+]
+export default SaltwaterFishData;
