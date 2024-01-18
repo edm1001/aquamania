691756c0f5ad0d3eda8dee06266033f3e7d9e3dd
edm1001 <edmer_franciz@yahoo.com>
2024-01-18 17:34:54 -0600

    worked on blog page

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -22,47 +22,46 @@ export default function Blog() {
   const [selectedTopic, setSelectedTopic] = useState(null);
 
   const handleTopicClick = (topicId) => {
-    setSelectedTopic(topicId);
+    setSelectedTopic(prevSelectedTopic => (prevSelectedTopic === topicId ? null : topicId));
   };
-  return (
-    <>
-      <Container className="text-center">
-        <h2>Talk about anything Aqua!</h2>
-        <p>We encourage this to be a safe space to debate or talk amongst fish lovers. Any hateful or insulting comments will result in deleted comments, topics, or account termination.</p>
-      </Container>
 
-      <Container>
-        <Row>
-          <Col as='div' className='w-100'>
-            <h3>Topics:</h3>
-            <ul className='subject-list'>
-              {topics.map(topic => (
-                <li key={topic.id}><a href={`#${topic.id}`} onClick={() => handleTopicClick(topic.id)}>{topic.name}</a></li>
-                ))}
-            </ul>
-          </Col>
-          
-          <Col as='div'>
-            {/* Render posts based on selected topic */}
+  return (
+    <Container>
+      <Row>
+        <Col as='div' className='w-100'>
+          <h3>Topics:</h3>
+          <ul className='subject-list'>
+            {topics.map(topic => (
+              <li key={topic.id}>
+                <a href={`#${topic.id}`} onClick={() => handleTopicClick(topic.id)}>
+                  {topic.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </Col>
+
+        <Col as='div'>
+          {/* Render posts based on selected topic */}
           {selectedTopic && (
             <div id={selectedTopic}>
               <h3>{topics.find(topic => topic.id === selectedTopic)?.name}</h3>
               <ul>
                 {posts
                   .filter(post => post.topic === selectedTopic)
-                  .map(filteredPost => (
-                    <li key={filteredPost.id}>{filteredPost.title}</li>
-                  ))}
+                  .map(filteredPost  => (
+                    // fix
+                    <li key={filteredPost.id}>{filteredPost.title}</li> 
+                    ))}
               </ul>
             </div>
-            )}
-          </Col>
-        </Row>
-        {/* make another row to have the topic and comments under it */}
-        {/* the post will have comment and likes functionality */}
+          )}
+        </Col>
+      </Row>
       </Container>
-
-      
-    </>
   );
 }
+/* make another row to have the topic and comments under it 
+   the post will have comment and likes functionality 
+   the 
+*/
