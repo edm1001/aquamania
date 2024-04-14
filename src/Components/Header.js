8c3dc1baa209066165ad2f0d04c5591352db9ce1
edm1001 <edmer_franciz@yahoo.com>
2024-04-14 13:12:46 -0500

    fix header bug & template

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,46 +1,52 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import Logo from '../assets/aquamania-logo.gif';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
-import { motion} from 'framer-motion';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Logo from "../assets/aquamania-logo.gif";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Header = () => {
-
-    return (
-      <motion.div
-        initial={{opacity:1}}
+  return (
+    <motion.div initial={{ opacity: 1 }}>
+      <Navbar
+        className="mb-5"
+        sticky="top"
+        bg="white"
+        data-bs-theme="light"
+        expand="lg"
       >
-      <Navbar className='mb-5' sticky='top' bg="white" data-bs-theme="light" expand="lg" >
         <Container>
-        <Navbar.Brand
-        href='/'
-        >
-          <img 
-          src={Logo}
-          width="80"
-          height="80"
-          alt='brand-logo'
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-        <Navbar.Collapse>
-        <Nav className=''>
-        <NavDropdown title="Fish" className='text-center'>
-          <Link to="/freshpage" className="dropdown-item">Freshwater Fish</Link>
-          <Link to="/category" className="dropdown-item">Saltwater Fish</Link>
-        </NavDropdown>
-        <Nav.Link to="/products" className='text-center'>Shop Page</Nav.Link>
-        <Nav.Link href='/about' className='text-center'>About Us</Nav.Link>
-        <Nav.Link href='/blog' className='text-center'>Blogs</Nav.Link>
-        </Nav>
-        </Navbar.Collapse>
+          <Navbar.Brand href="/">
+            <img src={Logo} width="80" height="80" alt="brand-logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav className="">
+              <NavDropdown title="Fish" className="text-center">
+                <Link to="/freshpage" className="dropdown-item text-center">
+                  Freshwater Fish
+                </Link>
+                <Link to="/category" className="dropdown-item text-center">
+                  Saltwater Fish
+                </Link>
+              </NavDropdown>
+              <Nav.Link href="/products" className="text-center">
+                Shop Page
+              </Nav.Link>
+              <Nav.Link href="/about" className="text-center">
+                About Us
+              </Nav.Link>
+              <Nav.Link href="/blog" className="text-center">
+                Blogs
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-      </motion.div>
-    )
-}
+    </motion.div>
+  );
+};
 
 export default Header;
