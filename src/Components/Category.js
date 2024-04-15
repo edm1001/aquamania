6264e4e43e7ca43972ec3ad3e64d43fc9a9cd087
edm1001 <edmer_franciz@yahoo.com>
2024-04-15 13:47:22 -0500

    style category component

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 import { Col, Row } from "react-bootstrap";
 
-const Category =() => {
+const Category = () => {
   const buttonVariants = {
     rest: {
       scale: 1,
@@ -12,33 +12,56 @@ const Category =() => {
     hover: {
       scale: 1.2,
       rotateX: 10,
-      boxShadow: '0 0 20px rgba(0, 0, 0, 0.3)',
+      boxShadow: "0 0 20px rgba(0, 0, 0, 0.3)",
     },
   };
   return (
-    <Container className="py-5">
-    <Row className="text-center">
-    <Col className="text-start"><h4>Select Your Region:</h4></Col>
-    <Col>
-    <motion.button 
-       className="3d-button mx-4 btn btn-success"
-       variants={buttonVariants}
-       whileHover="hover"
-       whileTap="rest"
-       >
-          <Link to="/freshpage" className="text-white" style={{textDecoration:'none'}}>Freshwater</Link>
-        </motion.button>
-        <motion.button 
-       className="3d-button mx-4 btn btn-info"
-       variants={buttonVariants}
-       whileHover="hover"
-       whileTap="rest"
-       >
-          <Link to="/saltpage" className="text-white" style={{textDecoration:'none'}}>Saltwater</Link>
-        </motion.button>
-       </Col>
-    </Row>
-  </Container>
-  )
-} 
+    <Container>
+      <Row className="py-5 justify-content-center align-items-center">
+        <Col xl={6} lg={6} md={6} sm={12} xs={12} className="text-center">
+          <div className="">
+            <h4 className="">Select your side of the Waters:</h4>
+          </div>
+          <div className="">
+            <motion.button
+              className="3d-button btn btn-success me-3"
+              variants={buttonVariants}
+              whileHover="hover"
+              whileTap="rest"
+            >
+              <Link
+                to="/freshpage"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                Freshwater
+              </Link>
+            </motion.button>
+            <motion.button
+              className="3d-button btn btn-info"
+              variants={buttonVariants}
+              whileHover="hover"
+              whileTap="rest"
+            >
+              <Link
+                to="/saltpage"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                Saltwater
+              </Link>
+            </motion.button>
+          </div>
+        </Col>
+        <Col xl={6} lg={6} md={6} className="col-6">
+          <img
+            src="https://via.placeholder.com/600x400"
+            alt=""
+            className="ctgry-img img-fluid"
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 export default Category;
