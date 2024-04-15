ed0bd3834aab3f367fbf36c4bd4b68e2b2562878
edm1001 <edmer_franciz@yahoo.com>
2024-04-15 13:17:50 -0500

    style single page

--- src/Pages/SpeciesDetail.jsx
+++ src/Pages/SpeciesDetail.jsx
@@ -1,22 +1,43 @@
-import {useParams} from 'react-router-dom';
-import freshwaterFishSpecies from '../db/Fresh/FeshwaterFishData.json';
+import { useParams } from "react-router-dom";
+import freshwaterFishSpecies from "../db/Fresh/FeshwaterFishData.json";
+import { Col, Row, Container } from "react-bootstrap";
 
 const SpeciesDetail = () => {
-    const { name } = useParams();
-    const selectedSpecies = freshwaterFishSpecies.find(species => species.name === decodeURIComponent(name));
+  const { name } = useParams();
+  const selectedSpecies = freshwaterFishSpecies.find(
+    (species) => species.name === decodeURIComponent(name)
+  );
 
   if (!selectedSpecies) {
     return <div>Species not found</div>;
   }
 
   return (
-    <div>
-      <h1>{selectedSpecies.name}</h1>
-      <img src={selectedSpecies.img} alt={selectedSpecies.name} />
-      <p>{selectedSpecies.description}</p>
-      <p>Temperament: {selectedSpecies.temperament}</p>
-      <p>Max Growth Size: {selectedSpecies.maxGrowthSize}</p>
-    </div>
+    <Container>
+      <Row>
+        <div className="d-flex">
+        <h1>{selectedSpecies.name}</h1>
+        <p className="ms-3 species-info">{selectedSpecies.species}</p>
+        </div>
+        <Col xl={6} lg={6} md={6} sm={12} xs={12}>
+          <img
+            src={selectedSpecies.img}
+            alt={selectedSpecies.name}
+            className="img-fluid"
+            style={{ maxWidth: '100%', height: 'auto' }}
+          />
+        </Col>
+        <Col xl={6} lg={6} md={6} sm={12} xs={12} className=" pt-3">
+          <Row>
+          <p className="col-6 fw-semibold species-info">Temperament: {selectedSpecies.temperament}</p>
+          <p className="col-6 fw-semibold species-info">Max Growth Size: {selectedSpecies.maxGrowthSize}</p>
+          <p className="col-6 fw-semibold species-info">Temperature Range: {selectedSpecies.temperatureRange}</p>
+          <p className="col-6 fw-semibold species-info">pH Range: {selectedSpecies.waterPHRange}</p>
+          </Row>
+          <p className="mt-1 fs-6 text-start">{selectedSpecies.description}</p>
+        </Col>
+      </Row>
+    </Container>
   );
-  };
-  export default SpeciesDetail;
+};
+export default SpeciesDetail;
