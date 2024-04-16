71af3ad19e4d81aea8b77b3383ef6d401f1ba768
edm1001 <edmer_franciz@yahoo.com>
2024-04-15 19:13:29 -0500

    add styling to about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,40 +1,65 @@
-import React from 'react'
-import Category from '../Components/Category';
-import {Row, Col, Container} from 'react-bootstrap'
+import React from "react";
+import Category from "../Components/Category";
+import { Row, Col } from "react-bootstrap";
 
 function About() {
   return (
-    <Container>
-    <Row>
-    <Col>
-    <img
+    <>
+      <Row>
+        <Col xl={6} lg={6} md={6} className="d-flex align-items-center justify-content-center">
+          <img
             src="https://via.placeholder.com/600x400"
             alt=""
-            className="img-fluid"
-          /></Col>
-      <Col className='text-center p-2'>
-      <h2 className='mb-3'>About Aquamania</h2>
-      <p className='px-1'>
-      Welcome to Aquamania, where our passion for aquatic life meets a commitment to providing comprehensive and reliable information on fish care. At Aquamania, we believe that every fish enthusiast, from beginners to seasoned hobbyists, deserves access to accurate and practical guidance. Our team of passionate enthusiasts has come together to create a hub of knowledge covering everything from choosing the right fish species to maintaining a thriving aquarium ecosystem and to help avoid fish abuse. Whether you're embarking on your first fish-keeping journey or looking to expand your aquatic expertise, Aquamania is here to be your go-to resource. Dive in and explore the fascinating world of fish care with confidence, backed by our commitment to quality content and community support.
-      </p>
-    </Col>
-    </Row>
-    <Category/>
-    <Row>
-      <Col>          <img
-            src="https://via.placeholder.com/600x400"
-            alt=""
-            className="ctgry-img img-fluid"
-          /></Col>
-    <Col className='text-center mx-1'>
-      <h2>Basic Care</h2>
-      <p>
-      This company was founded with the primary objective of educating individuals and fostering a high quality of life for our pet fish. Aquariums, recognized for their stress-reducing benefits, promote our well-being. Therefore, maintaining a clean and healthy aquarium will result in happier fish and happier you. If you are a new to this hobby, welcome and please read about multi-tank syndrome!</p>
-      <p> The key to achieving a thriving aquarium is maintaining stable parameters. Whatever parameters—such as pH or temperature—favor the well-being of your fish should be preserved without unnecessary alterations. Pursuing specific water parameters is generally unnecessary unless one is actively engaged in fish breeding. Keeping fish in a stable and well-balanced environment, consistent with their natural habitat, contributes significantly to their contentment and overall well-being.</p>
-    </Col>
-    </Row>
-    </Container>
-  )
+            className="img-fluid h-75 about-img"
+          />
+        </Col>
+        <Col className="text-center p-5">
+          <h2 className="mb-3">About Aquamania</h2>
+          <p className="px-1 about-page-info">
+            Welcome to Aquamania! We're all about sharing our love for fish and
+            ensuring you have reliable info for their care. From newbies to
+            pros, everyone deserves accurate advice. Our team of enthusiasts is
+            dedicated to covering everything from choosing fish to ecosystem
+            maintenance, all to prevent fish abuse. Whether you're starting out
+            or leveling up, Aquamania's got your back. Dive in and discover fish
+            care confidently, with our quality content and supportive community.
+          </p>
+        </Col>
+      </Row>
+      <Category />
+      <Row>
+        <Col xl={6} lg={6} md={6} className="d-flex align-items-center justify-content-center ">
+          <div className="bg-success">
+            <img
+              src="https://via.placeholder.com/600x400"
+              alt=""
+              className="img-fluid h-75 about-img"
+            />
+          </div>
+        </Col>
+        <Col className="text-center mx-1 bg-success text-white p-5">
+          <h2>Basic Care</h2>
+          <p className="about-page-info">
+            We're all about aquarium fish care! Our mission is to educate and
+            improve the lives of our finned friends. Aquariums aren't just
+            decorations; they're stress-busters for us too. Keeping your tank
+            clean and healthy means happier fish and a happier you. If you're
+            new to this, welcome aboard! And don't forget to watch out for
+            multi-tank syndrome!
+          </p>
+          <p className="about-page-info">
+            {" "}
+            What I believe to be a secret to a thriving aquarium? Stability is
+            key. Whether it's pH, temperature, or other factors, keeping your
+            water consistent is the most vital for fish wellness. Unless you're
+            breeding, chasing specific water conditions isn't necessary.
+            Creating an environment that mirrors their natural habitat is what
+            really counts for fish contentment and health.
+          </p>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default About
+export default About;
