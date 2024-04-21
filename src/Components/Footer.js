7281e7c04bb3fd3a694f045c16711613d23d5e4f
edm1001 <edmer_franciz@yahoo.com>
2024-04-20 21:35:09 -0500

    add styling to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,31 +7,32 @@ const Footer = () => {
       <Col xl={4} lg={4} md={4} sm={4} xs={4}>
         <img src={Logo} alt="logo" className="img-fluid"></img>
       </Col>
-      <Col xl={8} lg={8} md={8} sm={8} xs={8} className="footer-links d-flex flex-row">
-        <div className="fish-footer">
+      <Col xl={8} lg={8} md={8} sm={8} xs={8} className="mt-4 footer-links">
+        <div className="link-column">
           <h6>Fish</h6>
-          <div className="flex-link-column">
-          <a href="/freshpage" className="footer-link text-secondary">
-            Freshwater
-          </a>
-          <a href="/saltpage" className="footer-link text-secondary">
-            Saltwater
-          </a>
-          </div>
+          <p>
+            <a href="/freshpage" className="footer-link text-secondary">
+              Freshwater
+            </a>
+          </p>
+          <p>
+            <a href="/saltpage" className="footer-link text-secondary">
+              Saltwater
+            </a>
+          </p>
         </div>
-        <div className="">
+        <div className="link-column">
           <h6>Products</h6>
-          <p>Recommended Products</p>
-          <p>Featured Products</p>
+          <p> <a href='/products' className="footer-link text-secondary"> Recommended Products</a></p>
+          <p> <a href='/products' className="footer-link text-secondary">Featured Products</a></p>
         </div>
-        <div className="">
+        <div className="link-column">
           <h6>About Us</h6>
-          <p>About Aquamania</p>
-          <p>Basic Fish Care</p>
+          <p><a href="/about" className="footer-link text-secondary">About Aquamania</a></p>
+          <p><a href="/about" className="footer-link text-secondary">Our Mission</a></p>
         </div>
       </Col>
     </Row>
   );
 };
-
 export default Footer;
