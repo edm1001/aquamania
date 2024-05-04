2ec0390569da5fa8ae68e71675b14c816b9d81ec
edm1001 <edmer_franciz@yahoo.com>
2024-05-03 23:38:31 -0500

    add color scheme

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,18 +14,18 @@ const About = () => {
             variants={fadeIn}
             initial="hidden"
             animate="visible"
-            className='text-center h-100 w-100 py-5 px-3 bg-success text-white'
+            className='text-center h-100 w-100 py-5 px-3 abt-section'
         >
-            <h1 className='fw-bold text-warning'>About Aquamania</h1>
+            <h1 className='fw-bold abt-title'>About Aquamania</h1>
         <motion.div
             initial={{opacity:0}}
             whileInView={{opacity:1,transition:1}}
             className='py-2'
         >
             <p>
-            I created Aquamania as a go-to source for most fishz aquatic hobby with basic needs. in the hobby whether you're into freshwater or marine aquariums, this site is dedicated to provide the best information & tips.<br/>
+            I created Aquamania as a go-to source for most fish aquatic hobby with basic needs. in the hobby whether you're into freshwater or marine aquariums, this site is dedicated to provide the best information & tips.<br/>
             Whether starting out or a seasoned pro, we're here to share the passion and knowledge with all. </p>
-            <Badge pill bg="info">Get in touch with us to be featured!</Badge>
+            <span className='badge abt-badge'>Get in touch with us to be featured!</span>
         </motion.div>
         </motion.div>
     );
