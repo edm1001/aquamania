e25dde41497ed0a11d0b2e60cf2ab59abf8c935b
edm1001 <edmer_franciz@yahoo.com>
2024-05-18 14:55:26 -0500

    refactor add to cart handle

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const productsRouter = require('./routes/ProductsRoute.js');
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(bodyParser.json());
@@ -15,7 +16,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!')
 })
 
-const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
