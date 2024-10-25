8b0d8b58935e634c745b80cab9093f7253c2227c
Edmer Valencia <edmerfranciz@gmail.com>
2024-10-24 23:44:44 -0500

    init water category

---
+++ client/src/Components/Category/WaterCategory.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
+
+
+function WaterCategory() {
+    const [hoveredTopic, setHoveredTopic] = useState(null);
+    const searchTopics = [
+        {
+          label: "Freshwater",
+          description:
+            "Explore freshwater aquariums with a variety of fish and plants.",
+          background:
+            "https://images.unsplash.com/photo-1665853463786-6a2e2a540446?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          page: "/freshpage",
+        },
+        {
+          label: "Saltwater",
+          description:
+            "Dive into stunning saltwater aquariums, coral reefs, and exotic fish",
+          background: "https://images.unsplash.com/photo-1524096613842-71a8c45f08a2?q=80&w=2260&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          page: "/saltpage",
+        }
+      ];
+  return (
+    <>
+          {/* Button Section */}
+          <Row className="welcome-buttons my-2">
+        {searchTopics.map((topic, index) => (
+          <Col
+            key={index}
+            className="welcome-col text-white my-3"
+            style={{ height: "100%" }}
+            onMouseEnter={() => setHoveredTopic(index)}
+            onMouseLeave={() => setHoveredTopic(null)}
+          >
+            <div
+              className={`welcome-button p-5 m-1 ${
+                hoveredTopic === index ? "hovered" : ""
+              }`}
+              style={{
+                height: "100%",
+                width: "100%",
+                backgroundImage:
+                  hoveredTopic === index ? `url(${topic.background})` : "none",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="description text-center">
+                <p>{topic.description}</p>
+              </div>
+              <Link
+                to={topic.page}
+                className="link-button text-decoration-none"
+              >
+                <div className="button-content">{topic.label}</div>
+              </Link>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default WaterCategory
--- client/src/Components/Welcome.js
+++ client/src/Components/Welcome.js
@@ -2,70 +2,56 @@ import React, { useState } from "react";
 import Row from "react-bootstrap/row";
 import Col from "react-bootstrap/col";
 import { Link } from "react-router-dom";
+import WaterCategory from "./Category/WaterCategory";
 
 const Welcome = () => {
-  const [hoveredTopic, setHoveredTopic] = useState(null);
+  
+    const coasterSlide = [
+      {
+        title: "Freshwater",
+        description:"Find the best tropical fish!",
+        img: "https://images.pexels.com/photos/17801533/pexels-photo-17801533/free-photo-of-close-up-of-a-texas-cichlid-fish.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      },
+      {
+        title: "Saltwater",
+        description:"Find the best marine fish!",
+        img: "https://images.pexels.com/photos/1112007/pexels-photo-1112007.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      },
+      {
+        title: "Care",
+        description:"Care tips for a successful aquarium",
+        img: "https://images.pexels.com/photos/26744114/pexels-photo-26744114/free-photo-of-hands-of-a-person-in-water-surrounded-by-fish.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      },
+      {
+        title: "Products",
+        description:"Find the best products for your aquarium",
+        img: "https://images.unsplash.com/photo-1665853463786-6a2e2a540446?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
 
-  const searchTopics = [
-    {
-      label: "Freshwater",
-      description:
-        "Explore freshwater aquariums with a variety of fish and plants.",
-      background:
-        "https://images.unsplash.com/photo-1665853463786-6a2e2a540446?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      page: "/freshpage",
-    },
-    {
-      label: "Saltwater",
-      description:
-        "Dive into stunning saltwater aquariums, coral reefs, and exotic fish",
-      background: "https://images.unsplash.com/photo-1524096613842-71a8c45f08a2?q=80&w=2260&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      page: "/saltpage",
-    },
-    // {
-    //   label: "Products",
-    //   description: "Learn essential fish care, from feeding to health.",
-    //   background: "https://via.placeholder.com/600x400",
-    // },
-  ];
+    ];
+    
   return (
     <>
-      {/* Button Section */}
-      <Row className="welcome-buttons my-2">
-        {searchTopics.map((topic, index) => (
-          <Col
-            key={index}
-            className="welcome-col text-white my-3"
-            style={{ height: "100%" }}
-            onMouseEnter={() => setHoveredTopic(index)}
-            onMouseLeave={() => setHoveredTopic(null)}
-          >
-            <div
-              className={`welcome-button p-5 m-1 ${
-                hoveredTopic === index ? "hovered" : ""
-              }`}
-              style={{
-                height: "100%",
-                width: "100%",
-                backgroundImage:
-                  hoveredTopic === index ? `url(${topic.background})` : "none",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-            >
-              <div className="description text-center">
-                <p>{topic.description}</p>
-              </div>
-              <Link
-                to={topic.page}
-                className="link-button text-decoration-none"
+    <WaterCategory/>
+      {/* Carousel Section */}
+      <Row className="my-4">
+        <Col>
+          <div className="welcome-carousel">
+            {coasterSlide.map((slide, index) => (
+              <div
+                key={index}
+                className={`slide ${index === 0 ? "active" : ""}`}                
               >
-                <div className="button-content">{topic.label}</div>
-              </Link>
-            </div>
-          </Col>
-        ))}
-      </Row>
+                <div className="slide-content"> 
+                  <h2>{slide.title}</h2>  
+                  <p>{slide.description}</p>
+                </div>
+                <img src={slide.img} alt={slide.title} />
+              </div>
+            ))}
+          </div>      
+        </Col>
+      </Row> 
     </>
   );
 };
