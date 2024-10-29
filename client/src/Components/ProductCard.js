94fd97c785e69b42dca5c251681c2c0e0527cc33
Edmer Valencia <edmerfranciz@gmail.com>
2024-10-28 23:59:42 -0500

    remove cart option

--- client/src/Components/ProductCard.js
+++ client/src/Components/ProductCard.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
-import { FaCartPlus, FaLink } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
 import ProductRating from "./ProductRating";
 
 const ProductCard = ({ product }) => {
@@ -14,27 +14,6 @@ const ProductCard = ({ product }) => {
   const handleClose = () => {
     setShow(false);
     setQuantity(1);
-  };
-
-  const handleAddToCart = async () => {
-    try {
-      const cartItem = { ...product, quantity };
-      const response = await fetch("http://localhost:5000/api/cart", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(cartItem),
-      });
-      if (!response.ok) {
-        const error = await response.json();
-        throw new Error(`Failed to add product to cart : ${error}`);
-      }
-      console.log("Product added to cart", product);
-      handleClose();
-    } catch (error) {
-      console.error("Trouble adding product to cart:", error);
-    }
   };
 
   return (
@@ -69,8 +48,7 @@ const ProductCard = ({ product }) => {
             </div>
           </div>
         </Card>
-
-        {/* Modal */} 
+        {/* Modal */}
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton />
           <Modal.Body closeButton>
@@ -98,32 +76,13 @@ const ProductCard = ({ product }) => {
                 </div>
                 <p className="mt-4">{product.description}</p>
               </div>
-              <div className="col-md-3 d-flex justify-content-between align-items-center">
-                <div className="d-flex">
-                  <Form.Group className="mb-3">
-                    <Form.Label>Quantity</Form.Label>
-                    <Form.Control
-                      type="number"
-                      value={quantity}
-                      min="1"
-                      onChange={(e) => setQuantity(parseInt(e.target.value))}
-                    />
-                  </Form.Group>
-                </div>
-                <div className="d-flex justify-content-end align-items-center">
-                  <FaLink
-                    title="Link to Product"
-                    style={{ color: "#007ea7", cursor: "pointer" }}
-                    size={30}
-                    className="me-2"
-                  />
-                  <FaCartPlus
-                    title="Make a Wishlist on Amazon!"
-                    style={{ color: "#007ea7", cursor: "pointer" }}
-                    size={30}
-                    onClick={handleAddToCart}
-                  />
-                </div>
+              <div className="d-flex justify-content-end px-2">
+                <FaLink
+                  title="Link to Product"
+                  style={{ color: "#007ea7", cursor: "pointer" }}
+                  size={30}
+                  className="me-2"
+                />
               </div>
             </div>
           </Modal.Body>
