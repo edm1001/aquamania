b25578099d6ceeb7edfb60860793af4afda491e9
Edmer Valencia <edmerfranciz@gmail.com>
2025-01-15 21:27:27 -0600

    prepare for deployment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+const productsRoute = require("./routes/ProductsRoute.js");
+const cartRoute = require("./routes/CartRoute.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,9 +13,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // API Routes
-const productsRoute = require("./routes/ProductsRoute.js");
-const cartRoute = require("./routes/CartRoute.js");
-
 app.use("/api/products", productsRoute);
 app.use("/api/cart", cartRoute);
 
@@ -32,4 +31,3 @@ if (process.env.NODE_ENV === "production") {
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
